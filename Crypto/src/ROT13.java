26a114bf42c55bcaff1ebe9effe4060035c6f16f
Abiel Figueroa <Abe@zipcodes-MacBook-Pro-2.local>
2021-03-26 18:40:46 -0400

    Lab done

--- Crypto/src/ROT13.java
+++ Crypto/src/ROT13.java
@@ -3,8 +3,19 @@ import static java.lang.Character.isUpperCase;
 import static java.lang.Character.toLowerCase;
 
 public class ROT13  {
+    String lowerAlphabet="abcdefghijklmnopqrstuvwxyz";
+    String higherC1;
+    String higherC2;
+    String code1;
+    String code2;
+
+
 
     ROT13(Character cs, Character cf) {
+        code1=rotate(lowerAlphabet,cs);
+        code2=rotate(lowerAlphabet,cf);
+        higherC1=code1.toUpperCase();
+        higherC2=code2.toUpperCase();
     }
 
     ROT13() {
@@ -13,20 +24,33 @@ public class ROT13  {
 
     public String crypt(String text) throws UnsupportedOperationException {
 
-        return "";
+        return encrypt(text);
     }
 
     public String encrypt(String text) {
-        return text;
+        String temp ="";
+        for(int i=0; i<text.length();i++){
+            if (Character.isUpperCase(text.charAt(i))){
+            temp=temp.concat(Character.toString(higherC2.charAt(higherC1.indexOf(text.charAt(i)))));
+            }else if(Character.isLowerCase(text.charAt(i))) {temp=temp.concat(Character.toString(code2.charAt(code1.indexOf(text.charAt(i)))));
+            }else temp=temp.concat(Character.toString(text.charAt(i)));
+        }
+        return temp;
     }
 
     public String decrypt(String text) {
-        return text;
+        String temp ="";
+        for(int i=0; i<text.length();i++){
+            if (Character.isUpperCase(text.charAt(i))){
+                temp=temp.concat(Character.toString(higherC1.charAt(higherC2.indexOf(text.charAt(i)))));
+            }else if(Character.isLowerCase(text.charAt(i))) {temp=temp.concat(Character.toString(code1.charAt(code2.indexOf(text.charAt(i)))));
+            }else temp=temp.concat(Character.toString(text.charAt(i)));
+        }
+        return temp;
     }
 
     public static String rotate(String s, Character c) {
-
-        return "";
+        return s.substring(s.indexOf(c))+s.substring(0,s.indexOf(c));
     }
 
 }
